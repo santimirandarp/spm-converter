ddaafc52c00bc5363a33cacbcce8f60f2a08f339
Santi Miranda <santimir@protonmail.com>
2022-10-17 20:27:05 +0200

    chore: updated snapshot

--- src/__tests__/header.test.ts
+++ src/__tests__/header.test.ts
@@ -1,8 +1,6 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
-import { IOBuffer } from 'iobuffer';
-
 import { readHeader } from '../header';
 
 const filePath = join(__dirname, 'data', 'AFM', 'AFM.spm');
@@ -11,7 +9,6 @@ const buffer = readFileSync(filePath);
 describe('read header', () => {
   it('return the header', () => {
     const header = readHeader(buffer);
-    console.log(header)
     expect(header['Ciao image list']).toHaveLength(2);
     expect(header['Scanner list']).toHaveLength(1);
     expect(header).toMatchSnapshot();
